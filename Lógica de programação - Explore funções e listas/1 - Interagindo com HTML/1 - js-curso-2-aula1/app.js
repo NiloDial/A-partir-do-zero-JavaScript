8dbe3ab3947415b4e91e0532b1d26d152f88f2b0
NiloDial <nilodias97@gmail.com>
2025-04-29 14:46:08 -0300

    atualizando projeto Mod 2

--- Lógica de programação - Explore funções e listas/1 - Interagindo com HTML/1 - js-curso-2-aula1/app.js
+++ Lógica de programação - Explore funções e listas/1 - Interagindo com HTML/1 - js-curso-2-aula1/app.js
@@ -1,9 +1,26 @@
+/*
 var titulo = document.querySelector("h1");
 titulo.innerHTML = "Jogo do número secreto";
 
 var paragrafo = document.querySelector("p");
 paragrafo.innerHTML = "Escolha um número entre 1 e 10";
+*/
+
+var numeroSecreto = gerarNumeroAleatorio();
+
+function exiberTextoNaTela(tag, texto) {
+  var campo = document.querySelector(tag);
+  campo.innerHTML = texto;
+}
+
+exiberTextoNaTela("h1", "Jogo do número secreto");
+exiberTextoNaTela("p", "Escolha um número entre 1 e 10");
 
 function verificarChute() {
-  console.log("Apertou");
+  var chute = document.querySelector("input").value;
+  console.log(numeroSecreto);
+}
+
+function gerarNumeroAleatorio() {
+  return parseInt(Math.random() * 10 + 1);
 }
